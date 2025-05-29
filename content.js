6dbaec92f408244b03cf2bacc3b8957e55bcc25f
Kxpiyush <kxpiyush@gmail.com>
2025-05-29 16:00:23 -0400

    Remove credit and host system dependencies

--- content.js
+++ content.js
@@ -1,251 +1,199 @@
-document['addEventListener']('DOMContentLoaded', async function () {
-    const a = await new Promise(w => chrome['management']['getSelf'](x => w(x['version'])));
-    document['getElementById']('version')['innerText'] = '(version\x20v' + a + ')';
-    const b = {
-            'Acheson': {
-                'lat': 53.548701,
-                'lng': -113.76261
-            },
-            'Ajax': {
-                'lat': 43.850814,
-                'lng': -79.020296
-            },
-            'Balzac': {
-                'lat': 51.212985,
-                'lng': -114.007862
-            },
-            'Bolton': {
-                'lat': 43.875473,
-                'lng': -79.734437
-            },
-            'Brampton': {
-                'lat': 43.685271,
-                'lng': -79.759924
-            },
-            'Calgary': {
-                'lat': 51.045113,
-                'lng': -114.057141
-            },
-            'Cambridge': {
-                'lat': 43.361621,
-                'lng': -80.314429
-            },
-            'Concord': {
-                'lat': 43.80011,
-                'lng': -79.48291
-            },
-            'Dartmouth': {
-                'lat': 44.67134,
-                'lng': -63.57719
-            },
-            'Edmonton': {
-                'lat': 53.54545,
-                'lng': -113.49014
-            },
-            'Etobicoke': {
-                'lat': 43.65421,
-                'lng': -79.56711
-            },
-            'Hamilton': {
-                'lat': 43.25549,
-                'lng': -79.873376
-            },
-            'Mississauga': {
-                'lat': 43.58882,
-                'lng': -79.644378
-            },
-            'Nisku': {
-                'lat': 53.337845,
-                'lng': -113.531304
-            },
-            'Ottawa': {
-                'lat': 45.425226,
-                'lng': -75.699963
-            },
-            'Rocky\x20View': {
-                'lat': 51.18341,
-                'lng': -113.93527
-            },
-            'Scarborough': {
-                'lat': 43.773077,
-                'lng': -79.257774
-            },
-            'Sidney': {
-                'lat': 48.650629,
-                'lng': -123.398604
-            },
-            'ST.\x20Thomas': {
-                'lat': 42.777414,
-                'lng': -81.182973
-            },
-            'Stoney\x20Creek': {
-                'lat': 43.21681,
-                'lng': -79.76633
-            },
-            'Toronto': {
-                'lat': 43.653524,
-                'lng': -79.383907
-            },
-            'Vancouver': {
-                'lat': 49.261636,
-                'lng': -123.11335
-            },
-            'Vaughan': {
-                'lat': 43.849270138,
-                'lng': -79.535136594
-            },
-            'Whitby': {
-                'lat': 43.897858,
-                'lng': -78.943434
-            },
-            'Windsor': {
-                'lat': 42.317438,
-                'lng': -83.035225
-            }
-        }, c = [
-            'Bolton',
-            'Brampton',
-            'Burnaby',
-            'Cambridge',
-            'Concord',
-            'Toronto',
-            'Sidney'
-        ], d = await chrome['storage']['local']['get']([
-            'selectedCity',
-            'distance',
-            'jobType',
-            '__ap',
-            'cityTags'
-        ]), e = d['selectedCity'] || 'Toronto', f = d['distance'] || '5', g = d['jobType'] || 'Any', h = d['__ap'] || ![], i = d['cityTags'] || [];
-    await chrome['storage']['local']['get']()['then'](w => {
-        const x = document['getElementById']('activate');
-        x ? x['checked'] = w['__ap'] : console['error']('Element\x20with\x20ID\x20\x22activate\x22\x20not\x20found.');
+document.addEventListener('DOMContentLoaded', async function() {
+    const version = await new Promise(resolve => chrome.management.getSelf(x => resolve(x.version)));
+    document.getElementById('version').innerText = '(version v' + version + ')';
+
+    const cityCoordinates = {
+        'Acheson': { lat: 53.548701, lng: -113.76261 },
+        'Ajax': { lat: 43.850814, lng: -79.020296 },
+        'Balzac': { lat: 51.212985, lng: -114.007862 },
+        'Bolton': { lat: 43.875473, lng: -79.734437 },
+        'Brampton': { lat: 43.685271, lng: -79.759924 },
+        'Calgary': { lat: 51.045113, lng: -114.057141 },
+        'Cambridge': { lat: 43.361621, lng: -80.314429 },
+        'Concord': { lat: 43.80011, lng: -79.48291 },
+        'Dartmouth': { lat: 44.67134, lng: -63.57719 },
+        'Edmonton': { lat: 53.54545, lng: -113.49014 },
+        'Etobicoke': { lat: 43.65421, lng: -79.56711 },
+        'Hamilton': { lat: 43.25549, lng: -79.873376 },
+        'Mississauga': { lat: 43.58882, lng: -79.644378 },
+        'Nisku': { lat: 53.337845, lng: -113.531304 },
+        'Ottawa': { lat: 45.425226, lng: -75.699963 },
+        'Rocky View': { lat: 51.18341, lng: -113.93527 },
+        'Scarborough': { lat: 43.773077, lng: -79.257774 },
+        'Sidney': { lat: 48.650629, lng: -123.398604 },
+        'ST. Thomas': { lat: 42.777414, lng: -81.182973 },
+        'Stoney Creek': { lat: 43.21681, lng: -79.76633 },
+        'Toronto': { lat: 43.653524, lng: -79.383907 },
+        'Vancouver': { lat: 49.261636, lng: -123.11335 },
+        'Vaughan': { lat: 43.849270138, lng: -79.535136594 },
+        'Whitby': { lat: 43.897858, lng: -78.943434 },
+        'Windsor': { lat: 42.317438, lng: -83.035225 }
+    };
+
+    const defaultCities = [
+        'Bolton', 'Brampton', 'Burnaby', 'Cambridge',
+        'Concord', 'Toronto', 'Sidney'
+    ];
+
+    // Load saved settings
+    const savedSettings = await chrome.storage.local.get([
+        'selectedCity',
+        'distance',
+        'jobType',
+        '__ap',
+        'cityTags'
+    ]);
+
+    const selectedCity = savedSettings.selectedCity || 'Toronto';
+    const distance = savedSettings.distance || '5';
+    const jobType = savedSettings.jobType || 'Any';
+    const isActive = savedSettings.__ap || false;
+    const cityTags = savedSettings.cityTags || [];
+
+    // Initialize UI elements
+    const citySelect = document.getElementById('city');
+    const distanceSelect = document.getElementById('distance');
+    const workHoursSelect = document.getElementById('work_hours');
+    const activateToggle = document.getElementById('activate');
+
+    if (citySelect) citySelect.value = selectedCity;
+    if (distanceSelect) distanceSelect.value = distance;
+    if (workHoursSelect) workHoursSelect.value = jobType;
+    if (activateToggle) activateToggle.checked = isActive;
+
+    // Set coordinates for selected city
+    const { lat, lng } = cityCoordinates[selectedCity];
+    chrome.storage.local.set({ lat, lng });
+
+    // Event listeners
+    citySelect.addEventListener('change', function() {
+        const city = this.value;
+        const { lat, lng } = cityCoordinates[city];
+        chrome.storage.local.set({
+            selectedCity: city,
+            lat,
+            lng
+        });
     });
-    i['length'] === 0x0 ? chrome['storage']['local']['set']({ 'cityTags': c }, function () {
-        console['log']('Default\x20cities\x20added\x20to\x20local\x20storage:', c);
-    }) : console['log']('Stored\x20city\x20tags\x20already\x20exist:', i);
-    console['log']('Stored\x20data\x20on\x20load:', d), console['log']('Stored\x20city\x20tags:', i);
-    const j = document['getElementById']('city'), k = document['getElementById']('distance'), l = document['getElementById']('work_hours'), m = document['getElementById']('activate');
-    if (j)
-        j['value'] = e;
-    if (k)
-        k['value'] = f;
-    if (l)
-        l['value'] = g;
-    if (m)
-        m['checked'] = h;
-    const {
-        lat: n,
-        lng: o
-    } = b[e];
-    chrome['storage']['local']['set']({
-        'lat': n,
-        'lng': o
-    }), document['getElementById']('city')['addEventListener']('change', function () {
-        const w = this['value'], {
-                lat: x,
-                lng: y
-            } = b[w];
-        chrome['storage']['local']['set']({
-            'selectedCity': w,
-            'lat': x,
-            'lng': y
-        }), console['log']('Stored\x20city:\x20' + w + ',\x20Lat:\x20' + x + ',\x20Lng:\x20' + y);
-    }), document['getElementById']('distance')['addEventListener']('change', function () {
-        const w = this['value'];
-        chrome['storage']['local']['set']({ 'distance': w }), console['log']('Stored\x20distance:\x20' + w);
-    }), document['getElementById']('work_hours')['addEventListener']('change', function () {
-        const w = this['value'];
-        chrome['storage']['local']['set']({ 'jobType': w }), console['log']('Stored\x20job\x20type:\x20' + w);
-    }), document['getElementById']('activate')['addEventListener']('change', async function () {
-        chrome['storage']['local']['set']({ '__ap': this['checked'] });
-        let [w] = await chrome['tabs']['query']({
-            'active': !![],
-            'lastFocusedWindow': !![]
+
+    distanceSelect.addEventListener('change', function() {
+        chrome.storage.local.set({ distance: this.value });
+    });
+
+    workHoursSelect.addEventListener('change', function() {
+        chrome.storage.local.set({ jobType: this.value });
+    });
+
+    activateToggle.addEventListener('change', async function() {
+        chrome.storage.local.set({ '__ap': this.checked });
+        const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+        chrome.tabs.sendMessage(tab.id, {
+            action: 'activate',
+            status: this.checked
         });
-        chrome['tabs']['sendMessage'](w['id'], {
-            'action': 'activate',
-            'status': this['checked']
-        });
-    }), document['getElementById']('ais_visa_info')['addEventListener']('submit', async function (w) {
-        w['preventDefault']();
-        let x = document['getElementById']('reset_info');
-        x['setAttribute']('disabled', 'disabled'), await new Promise(y => setTimeout(y, 0x1f4)), await chrome['storage']['local']['clear'](), await chrome['storage']['local']['set']({
-            '__ap': !![],
-            '__cr': 0x0,
+    });
+
+    // Reset functionality
+    document.getElementById('ais_visa_info').addEventListener('submit', async function(e) {
+        e.preventDefault();
+        const resetButton = document.getElementById('reset_info');
+        resetButton.setAttribute('disabled', 'disabled');
+        
+        await chrome.storage.local.clear();
+        await chrome.storage.local.set({
+            '__ap': true,
             'selectedCity': 'Toronto',
             'lat': 43.653524,
             'lng': -79.383907,
             'distance': '5',
             'jobType': 'Any'
-        }), chrome['runtime']['sendMessage']({ 'action': 'logout' }), x['classList']['toggle']('btn-success'), x['innerText'] = 'Success', await new Promise(y => setTimeout(y, 0x3e8)), x['classList']['toggle']('btn-success'), x['removeAttribute']('disabled'), x['innerText'] = 'Reset';
-    }), document['getElementById']('contact_us')['addEventListener']('click', function () {
-        chrome['tabs']['create']({ 'url': 'https://www.alertmeasap.com/contact' });
-    }), document['getElementById']('tutorial')['addEventListener']('click', function () {
-        chrome['tabs']['create']({ 'url': 'https://youtu.be/17iWkjXwRJs' });
+        });
+
+        resetButton.classList.toggle('btn-success');
+        resetButton.innerText = 'Success';
+        
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        
+        resetButton.classList.toggle('btn-success');
+        resetButton.removeAttribute('disabled');
+        resetButton.innerText = 'Reset';
     });
-    function p(w, x = ![]) {
-        const y = document['getElementById']('tag-input-box'), z = document['createElement']('div');
-        z['classList']['add']('tag'), z['innerHTML'] = w + '\x20<span\x20class=\x22remove-tag\x22>x</span>', y['insertBefore'](z, document['getElementById']('city-input')), document['getElementById']('clear-all')['style']['display'] = 'inline', z['querySelector']('.remove-tag')['addEventListener']('click', function () {
-            q(this);
-        }), console['log']('Tag\x20added:\x20' + w), console['log']('Current\x20tags\x20in\x20DOM:\x20' + document['querySelectorAll']('.tag')['length']), !x && s(w);
-    }
-    function q(w) {
-        const x = w['parentElement'], y = x['textContent']['trim']()['slice'](0x0, -0x1);
-        x['remove'](), t(y), console['log']('Tag\x20removed\x20from\x20UI:\x20' + y), !document['querySelector']('.tag') && (document['getElementById']('clear-all')['style']['display'] = 'none');
-    }
-    function r() {
-        const w = document['querySelectorAll']('.tag');
-        w['forEach'](x => x['remove']()), u(), document['getElementById']('clear-all')['style']['display'] = 'none', console['log']('All\x20tags\x20cleared.');
-    }
-    function s(w) {
-        chrome['storage']['local']['get']('cityTags', function (x) {
-            let y = x['cityTags'] || [];
-            y['push'](w), chrome['storage']['local']['set']({ 'cityTags': y }, function () {
-                console['log']('Tag\x20saved\x20to\x20local\x20storage:\x20' + w), console['log']('Current\x20tags\x20in\x20storage:\x20' + y);
-            });
+
+    // Tutorial button
+    document.getElementById('tutorial').addEventListener('click', function() {
+        chrome.tabs.create({ url: 'https://youtu.be/17iWkjXwRJs' });
+    });
+
+    // Tag management functions
+    function addTag(tag, skipStorage = false) {
+        const tagBox = document.getElementById('tag-input-box');
+        const tagElement = document.createElement('div');
+        tagElement.classList.add('tag');
+        tagElement.innerHTML = tag + ' <span class="remove-tag">x</span>';
+        
+        tagBox.insertBefore(tagElement, document.getElementById('city-input'));
+        document.getElementById('clear-all').style.display = 'inline';
+        
+        tagElement.querySelector('.remove-tag').addEventListener('click', function() {
+            removeTag(this);
         });
-    }
-    function t(w) {
-        chrome['storage']['local']['get']('cityTags', function (x) {
-            let y = x['cityTags'] || [];
-            console['log']('Before\x20removal,\x20stored\x20city\x20tags:', y), y = y['filter'](z => z['trim']()['toLowerCase']() !== w['trim']()['toLowerCase']()), chrome['storage']['local']['set']({ 'cityTags': y }, function () {
-                console['log']('After\x20removal,\x20stored\x20city\x20tags:', y), chrome['storage']['local']['get']('cityTags', function (z) {
-                    console['log']('Final\x20check\x20of\x20stored\x20tags:\x20', z['cityTags']);
-                });
-            });
-        });
-    }
-    function u() {
-        chrome['storage']['local']['remove']('cityTags', function () {
-            console['log']('All\x20tags\x20cleared\x20from\x20local\x20storage.');
-        });
-    }
-    function v() {
-        const w = document['getElementById']('tag-input-box');
-        w['querySelectorAll']('.tag')['forEach'](x => x['remove']()), chrome['storage']['local']['get']('cityTags', function (x) {
-            const y = x['cityTags'] || [];
-            console['log']('Loading\x20tags\x20from\x20storage:\x20' + y), y['forEach'](z => p(z, !![]));
-        });
-    }
-    document['getElementById']('clear-all')['addEventListener']('click', r), document['getElementById']('city-input')['addEventListener']('keyup', function (w) {
-        if (w['key'] === 'Enter' && this['value']['trim']() !== '') {
-            const x = this['value']['trim']();
-            p(x), this['value'] = '';
+
+        if (!skipStorage) {
+            saveTag(tag);
         }
-    }), chrome['runtime']['onMessage']['addListener']((w, x, y) => {
-        if (w['action'] === 'playSound') {
-            console['log']('Attempting\x20to\x20play\x20sound...');
-            const z = new Audio(chrome['runtime']['getURL']('alert.wav'));
-            z['play']()['then'](() => {
-                console['log']('Sound\x20played\x20successfully.');
-            })['catch'](A => {
-                console['error']('Failed\x20to\x20play\x20sound:', A);
-                const B = document['createElement']('button');
-                B['style']['display'] = 'none', document['body']['appendChild'](B), B['addEventListener']('click', () => {
-                    z['play']();
-                }), console['log']('Triggering\x20hidden\x20button\x20click.'), B['click'](), document['body']['removeChild'](B);
-            });
+    }
+
+    function removeTag(element) {
+        const tag = element.parentElement;
+        const tagText = tag.textContent.trim().slice(0, -1);
+        tag.remove();
+        removeTagFromStorage(tagText);
+        
+        if (!document.querySelector('.tag')) {
+            document.getElementById('clear-all').style.display = 'none';
         }
-    }), v();
+    }
+
+    function saveTag(tag) {
+        chrome.storage.local.get('cityTags', function(result) {
+            let tags = result.cityTags || [];
+            tags.push(tag);
+            chrome.storage.local.set({ cityTags: tags });
+        });
+    }
+
+    function removeTagFromStorage(tag) {
+        chrome.storage.local.get('cityTags', function(result) {
+            let tags = result.cityTags || [];
+            tags = tags.filter(t => t.trim().toLowerCase() !== tag.trim().toLowerCase());
+            chrome.storage.local.set({ cityTags: tags });
+        });
+    }
+
+    function clearAllTags() {
+        const tags = document.querySelectorAll('.tag');
+        tags.forEach(tag => tag.remove());
+        chrome.storage.local.remove('cityTags');
+        document.getElementById('clear-all').style.display = 'none';
+    }
+
+    // Initialize tags
+    function loadTags() {
+        chrome.storage.local.get('cityTags', function(result) {
+            const tags = result.cityTags || defaultCities;
+            tags.forEach(tag => addTag(tag, true));
+        });
+    }
+
+    // Event listeners for tag management
+    document.getElementById('clear-all').addEventListener('click', clearAllTags);
+    
+    document.getElementById('city-input').addEventListener('keyup', function(e) {
+        if (e.key === 'Enter' && this.value.trim() !== '') {
+            addTag(this.value.trim());
+            this.value = '';
+        }
+    });
+
+    loadTags();
 });
